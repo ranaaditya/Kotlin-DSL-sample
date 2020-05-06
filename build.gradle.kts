178922171579a15ca8ed0cc1c50dba6588f36e12
ranaaditya <mail2aditya.rana98@gmail.com>
2020-05-06 15:58:58 +0530

    Update Project level build.gradle File

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,15 +1,20 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
+// rename build.gradle -> build.gradle.kts
 
 buildscript {
-    ext.kotlin_version = '1.3.71'
+
     repositories {
         google()
         jcenter()
-        
+
     }
+
+
     dependencies {
-        classpath 'com.android.tools.build:gradle:3.6.0'
-        classpath "org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version"
+        // classpath becomes function in Kotlin dsl
+        // classpath "" -> classpath("")
+        classpath ("com.android.tools.build:gradle:3.6.0")
+        classpath ("org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.71")
 
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
@@ -24,6 +29,6 @@ allprojects {
     }
 }
 
-task clean(type: Delete) {
-    delete rootProject.buildDir
+tasks.register("clean", Delete::class) {
+    delete( rootProject.buildDir)
 }
