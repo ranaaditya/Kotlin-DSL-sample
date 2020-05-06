0fcabd9c38ce804be8f0db3ba645e70f9c9d5c40
ranaaditya <mail2aditya.rana98@gmail.com>
2020-05-06 15:00:26 +0530

    Update settings.gradle File

---
+++ settings.gradle.kts
@@ -0,0 +1,8 @@
+// rename file from settings.gradle -> settingd.gradle.kts
+
+// '' -> ""
+rootProject.name="Kotlin-DSL example"
+
+// incude -> include()
+// include becomes function in Kotlin-DSL
+include(":app")
